abafed16be1e30c567c864a6021612b4b1463801
nethma-iCET <icet.nethma@gmail.com>
2025-08-28 23:28:58 +0530

    create Room Management FXML layout

--- src/main/java/controller/DashboardFormController.java
+++ src/main/java/controller/DashboardFormController.java
@@ -1,4 +1,29 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class DashboardFormController {
+
+    Stage roomManagement = new Stage();
+
+    @FXML
+    private JFXButton btnRoomManagement;
+
+    @FXML
+    void btnRoomManagementOnAction(ActionEvent event) {
+        try {
+            roomManagement.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/RoomManagement.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        roomManagement.show();
+    }
+
 }
