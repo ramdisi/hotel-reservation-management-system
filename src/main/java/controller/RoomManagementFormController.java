002f367373771466f2c14399745ef46f2ee2a8c7
nethmakannangara <nethmakannangara007@gmail.com>
2025-09-05 19:07:04 +0530

    add fx:id attributes to FXML form elements

--- src/main/java/controller/RoomManagementFormController.java
+++ src/main/java/controller/RoomManagementFormController.java
@@ -1,4 +1,83 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXRadioButton;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class RoomManagementFormController {
+
+    @FXML
+    private JFXRadioButton availableRadio;
+
+    @FXML
+    private JFXButton btnAdd;
+
+    @FXML
+    private JFXButton btnClear;
+
+    @FXML
+    private JFXButton btnDelete;
+
+    @FXML
+    private JFXButton btnUpdate;
+
+    @FXML
+    private TableColumn<?, ?> colDescription;
+
+    @FXML
+    private TableColumn<?, ?> colPricePerNight;
+
+    @FXML
+    private TableColumn<?, ?> colRoomNumber;
+
+    @FXML
+    private TableColumn<?, ?> colRoomStatus;
+
+    @FXML
+    private TableColumn<?, ?> colRoomType;
+
+    @FXML
+    private JFXComboBox<?> roomTypeCombo;
+
+    @FXML
+    private TableView<?> tblRoomDetails;
+
+    @FXML
+    private JFXTextArea txtDescription;
+
+    @FXML
+    private JFXTextField txtPricePerNight;
+
+    @FXML
+    private JFXTextField txtRoomNumber;
+
+    @FXML
+    private JFXRadioButton unavailableRadio;
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnClearOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
 }
